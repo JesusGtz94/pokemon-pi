23aeb84525599f0fc66c424f3e33c2f8e26257cc
JesusGtz94 <jesusgtz.dev@gmail.com>
2021-11-09 14:44:56 -0600

    Mejoras al testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,10 +1,17 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
-
-
+import Card from './Components/card';
   
   //const linkElement = screen.getByText(/learn react/i);
-
+  const pokemon = {
+    id: 0,
+    name: 'Pedro',
+    img: '',
+    types: [{name:'fire'}],
+    attack: 1,
+    defense: 2,
+    hp: 3
+  }
 
     describe('Debe tener un boton que dirija al home', () => {
 
@@ -12,11 +19,69 @@ import App from './App';
       
       const button = screen.getByRole('button');
 
-
       it('El botón debe existir', () => {
 
         expect(button).toBeInTheDocument();
 
       })
 
+
+
     });
+
+    describe('Debe renderizar una card con los datos que recibe', () => {
+
+      render(<Card {...pokemon}/>);
+      
+      const id = screen.getByTestId("id");
+      const name = screen.getByTestId("name");
+      const img = screen.getByTestId("img");
+      const type = screen.getByTestId("type");
+      const attack = screen.getByTestId("attack");
+      const defense = screen.getByTestId("defense");
+      const hp = screen.getByTestId("hp");
+
+      it('Debe renderizar el id', () => {
+
+        expect(id.innerHTML.includes('0')).toBe(true);
+
+      })
+      
+      it('Debe renderizar el nombre', () => {
+
+        expect(name.innerHTML.includes('Pedro')).toBe(true);
+
+      })
+
+      it('Debe renderizar el tipo', () => {
+
+        expect(type.innerHTML.includes('fire')).toBe(true);
+
+      })
+
+      it('Debe renderizar el ataque', () => {
+
+        expect(attack.innerHTML.includes('1')).toBe(true);
+
+      })
+
+      it('Debe renderizar la defensa', () => {
+
+        expect(defense.innerHTML.includes('2')).toBe(true);
+
+      })
+
+      it('Debe renderizar la vida', () => {
+
+        expect(hp.innerHTML.includes('3')).toBe(true);
+
+      })
+
+      it('Debe renderizar el nombre del pokemon si no recibe una imagen', () => {
+
+        expect(img.alt.includes('Pedro')).toBe(true);
+
+      })
+
+    });
+
--- client/src/Components/card.jsx
+++ client/src/Components/card.jsx
@@ -21,25 +21,25 @@ const Card = ({id,name,img,types,attack,defense,hp}) => {
 
                 <div className="img-box back-box">
 
-                    <img src={img.length > 8 ? img : 'https://assets.thespinoff.co.nz/1/2019/04/HddtBOT.png'} alt={name} />
+                    <img data-testid="img" src={img.length > 8 ? img : 'https://assets.thespinoff.co.nz/1/2019/04/HddtBOT.png'} alt={name} />
                     
                 </div>
 
-                <p className="one-box back-box"> <span> Id: </span>{id} </p>
+                <p data-testid="id" className="one-box back-box"> <span> Id: </span>{id} </p>
 
                 <div className="two-box">
                     <div>
-                        <p className="back-box"> <span> Nombre </span><br/> {name} </p>
-                        <p className="back-box"><span>Tipo</span><br/> {types[0] ? types.join('/') : "?"}</p>
+                        <p data-testid="name" className="back-box"> <span> Nombre </span><br/> {name} </p>
+                        <p data-testid="type" className="back-box"><span>Tipo</span><br/> {types[0] ? types.join('/') : "?"}</p>
 
                     </div>
                 </div>
 
                 <div className="three-box"> 
                     <div>
-                        <p className="back-box"><span>Ataque</span><br/> {attack}</p>
-                        <p className="back-box"><span>Defensa</span><br/> {defense}</p>
-                        <p className="back-box"><span>Vida</span><br/> {hp}</p>
+                        <p data-testid="attack" className="back-box"><span>Ataque</span><br/> {attack}</p>
+                        <p data-testid="defense" className="back-box"><span>Defensa</span><br/> {defense}</p>
+                        <p data-testid="hp" className="back-box"><span>Vida</span><br/> {hp}</p>
                     </div>
                 </div>
         </DivCard>
